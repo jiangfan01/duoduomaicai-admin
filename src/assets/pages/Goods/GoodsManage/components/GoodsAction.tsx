5ebc4e9b0e0abd17f31f4eed0a4e600416fe25ba
Serein <1431037397@qq.com>
2024-10-18 21:35:43 +0800

    Feature:新增商品模块、团购修改

---
+++ src/assets/pages/Goods/GoodsManage/components/GoodsAction.tsx
@@ -0,0 +1,90 @@
+import React, {useEffect} from 'react';
+import {
+    Button,
+    Form,
+    Input, InputNumber,
+    Select,
+} from 'antd';
+import UploadImg from "../../../../components/upload.tsx";
+
+interface DataType {
+    key: React.Key;
+    name: string;
+    price: number;
+    image: string;
+    category: string;
+    describe: string
+}
+
+interface EditFormProps {
+    action: 'add' | 'edit' | 'check';
+    record: DataType | null;
+    onSubmit: () => void;
+}
+
+const category = [
+    {value: '1', label: '团购'},
+    {value: '2', label: '精选'},
+    {value: '3', label: '服务'},
+];
+const GoodsAction: React.FC<EditFormProps> = ({action, record, onSubmit}) => {
+    const handleFormSubmit = () => {
+        onSubmit();
+    };
+    const [form] = Form.useForm();
+
+    useEffect(() => {
+        if (record) {
+            form.setFieldsValue({
+                name: record.name,
+                price: record.price,
+                image: record.image,
+                category: record.category,
+                describe: record.describe,
+            });
+        } else {
+            form.resetFields();
+        }
+    }, [record, form]);
+
+    const isCheckMode = action === 'check';
+    return (
+        <Form
+            labelCol={{span: 4}}
+            wrapperCol={{span: 14}}
+            layout="horizontal"
+            form={form}
+            style={{maxWidth: 600}}
+        >
+            <Form.Item label="商品名" name="name">
+                <Input placeholder={'请输入商品名'} disabled={isCheckMode}/>
+            </Form.Item>
+            <Form.Item label="价格" name="price">
+                <InputNumber placeholder={'请输入价格'} disabled={isCheckMode} style={{width: 275}}/>
+            </Form.Item>
+            <Form.Item label={action === 'edit' ? '修改图片' : '上传'}>
+                <UploadImg action={action} initialImageUrl={record?.image}/>
+            </Form.Item>
+            <Form.Item label="分类" name="category">
+                <Select placeholder="请选择分类" disabled={isCheckMode}>
+                    {category.map((item) => (
+                        <Select.Option key={item.value} value={item.value}>
+                            {item.label}
+                        </Select.Option>
+                    ))}
+                </Select>
+            </Form.Item>
+            <Form.Item label="商品描述" name="describe">
+                <Input placeholder={'请输入描述'} disabled={isCheckMode}/>
+            </Form.Item>
+
+            <Form.Item label="操作">
+                <Button
+                    type="primary"
+                    onClick={handleFormSubmit}
+                >{action === 'check' ? '关闭' : '提交'}</Button>
+            </Form.Item>
+        </Form>
+    );
+};
+export default GoodsAction;
